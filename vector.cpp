8f48261d8e841c4556a0a55e3f8ec31b784ba775
Blaise NDIZEYE <blaiseendizeye@gmail.com>
2025-04-20 20:20:04 +0100

    Complete exercise 1 on Lab 9

---
+++ vector.cpp
@@ -0,0 +1,132 @@
+#include "vector.h"
+#include <iostream>
+using namespace std;
+
+Vector::Vector(int size) {
+  if (size <= 0)
+    throw invalid_argument("Please enter a valid size number");
+  this->size = size;
+  lastIndex = 0;
+  data = new int[size]{0};
+}
+
+Vector::~Vector() { delete[] data; }
+
+void Vector::setSize(const int &size) {
+  if (size == this->size)
+    return;
+
+  int *newData = new int[size];
+  int newLastIndex = 0;
+  int minSize = min(size, this->size);
+
+  for (newLastIndex = 0; newLastIndex < minSize; newLastIndex++) {
+    newData[newLastIndex] = data[newLastIndex];
+  }
+
+  if (size > this->size) {
+    for (int i = newLastIndex; i < size; i++) {
+      newData[i] = 0;
+    }
+  }
+
+  delete[] data;
+  this->size = size;
+  lastIndex = newLastIndex;
+  data = newData;
+}
+
+void Vector::setAll(const int size, const int *newData) {
+  setSize(size);
+  for (int i = 0; i < size; i++) {
+    data[i] = newData[i];
+  }
+}
+
+void Vector::add(const int &el) {
+  if (lastIndex == this->size)
+    setSize(size + 1);
+
+  data[lastIndex] = el;
+  lastIndex++;
+}
+
+void Vector::print() const {
+  cout << "(";
+  for (int i = 0; i < size; i++) {
+    cout << data[i] << (i + 1 == size ? "" : ", ");
+  }
+  cout << ") \n";
+}
+
+void Vector::sumVect(const Vector &v) {
+  if (v.getSize() != size)
+    throw invalid_argument("Only vectors of the same size can be added!");
+
+  for (int i = 0; i < size; i++) {
+    data[i] = data[i] + v.data[i];
+  }
+}
+
+void Vector::prodVect(const Vector &v) {
+  if (v.getSize() != size)
+    throw invalid_argument("Only vectors of the same size can be multiplied!");
+
+  for (int i = 0; i < size; i++) {
+    data[i] = data[i] * v.data[i];
+  }
+}
+
+const int *Vector::getData() const { return data; }
+
+int Vector::getSize() const { return size; }
+
+BitVector::BitVector(int size) : Vector(size) {}
+
+void BitVector::validateBitNumber(const int &el) const {
+  string elStr = to_string(el);
+
+  for (char c : elStr) {
+    if (c != '0' && c != '1')
+      throw invalid_argument(
+          "Only numbers with 1s and 0s are allowed in BitVector");
+  }
+}
+
+void BitVector::setAll(const int size, const int *newData) {
+  for (int i = 0; i < size; i++)
+    validateBitNumber(newData[i]);
+
+  Vector::setAll(size, newData);
+}
+
+void BitVector::add(const int &el) {
+  validateBitNumber(el);
+  Vector::add(el);
+}
+
+void BitVector::sumVect(const Vector &v) {
+  if (v.getSize() != getSize())
+    throw invalid_argument("Only vectors of the same size can be added!");
+
+  int *summedData = new int[v.getSize()];
+  for (int i = 0; i < getSize(); i++) {
+    summedData[i] = getData()[i] | v.getData()[i];
+  }
+
+  setAll(getSize(), summedData);
+  delete[] summedData;
+}
+
+void BitVector::prodVect(const Vector &v) {
+  if (v.getSize() != getSize())
+    throw invalid_argument("Only vectors of the same size can be multiplied!");
+
+  int *multipliedData = new int[v.getSize()];
+  for (int i = 0; i < getSize(); i++) {
+    multipliedData[i] = getData()[i] & v.getData()[i];
+  }
+
+  setAll(getSize(), multipliedData);
+  delete[] multipliedData;
+}
