35a9e4b50a01249750c620b5a7456afa666c3b44
Blaise NDIZEYE <blaiseendizeye@gmail.com>
2025-05-06 10:28:23 +0100

    Implement the second subquestion of the 1st exercise

--- lib/pair.h
+++ lib/pair.h
@@ -4,11 +4,11 @@ using namespace std;
 
 template <class T, class R> class Pair {
 public:
-  Pair(const T &t, const R &u) : first(t), second(u) {}
-  T &get_first() { return first; }
-  R &get_second() { return second; }
-  const T &get_first() const { return first; }
-  const R &get_second() const { return second; }
+  Pair(const T &t, const R &u);
+  T &get_first();
+  R &get_second();
+  const T &get_first() const;
+  const R &get_second() const;
 
 private:
   T first;
@@ -17,4 +17,19 @@ private:
 
 template <class T, class R> void print_pair(const Pair<T, R> &p) {
   std::cout << '(' << p.get_first() << ", " << p.get_second() << ")\n";
+}
+
+template <class T, class R>
+Pair<T, R>::Pair(const T &t, const R &u) : first(t), second(u) {}
+
+template <class T, class R> T &Pair<T, R>::get_first() { return first; }
+
+template <class T, class R> R &Pair<T, R>::get_second() { return second; }
+
+template <class T, class R> const T &Pair<T, R>::get_first() const {
+  return first;
+}
+
+template <class T, class R> const R &Pair<T, R>::get_second() const {
+  return second;
 }
